9ffd872726d8c105b32d45e4d969f13ee2de76b6
sawyer <sawyerbowerman2@gmail.com>
2025-02-11 11:10:12 -0500

    Update Homework 1.

--- hw1.v
+++ hw1.v
@@ -80,8 +80,17 @@ then both boolean values are equal.
 Theorem ex5:
   forall (b c : bool), (andb b c = orb b c) -> b = c.
 Proof.
-
-Admitted.
+intros.
+destruct b.
+- assert (H' : forall (b : bool), orb true b = true). { reflexivity. }
+  rewrite H' in H.
+  rewrite <- H.
+  reflexivity.
+- assert (H': forall (b : bool), andb false b = false). { reflexivity. }
+  rewrite H' in H.
+  rewrite -> H.
+  reflexivity.
+Qed.
 
 
 (**
@@ -94,8 +103,16 @@ the outer most.
 Theorem ex6:
   forall n m, n + S m = S (n + m).
 Proof.
+intros.
+induction n.
 
-Admitted.
+- simpl.
+    reflexivity.
+
+- simpl.
+    rewrite IHn.
+    reflexivity.
+Qed.
 
 (**
 
@@ -104,13 +121,23 @@ are equal, then the numbers to the right of each addition must be equal.
 To complete this exercise you will need to use the auxiliary
 theorem: eq_add_S
 
-
  *)
+Check eq_add_S.
 Theorem ex7:
   forall x y n, n + x = n + y -> x = y.
 Proof.
+intros.
+induction n.
 
-Admitted.
+- simpl in H.
+    rewrite H.
+    reflexivity.
+
+- simpl in H. apply eq_add_S in H.
+    apply IHn.
+    rewrite H.
+    reflexivity.
+Qed.
 
 (**
 
@@ -120,11 +147,42 @@ separately.
 
 
  *)
+
+Lemma x_plus0 x :
+  x + 0 = x.
+Proof.
+  induction x.
+  - simpl. reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
+
+Lemma x_plusSy x y :
+  x + S y = S (x + y).
+Proof.
+  induction x.
+  - simpl. reflexivity.
+  - simpl. rewrite IHx. reflexivity.
+Qed.
+
 Theorem ex8:
   forall x y, x + y = y + x.
 Proof.
-
-Admitted.
+  intros x y.
+  induction x.
+  - (* Case: 0 + y = y + 0 *)
+    simpl.
+    assert (H: y + 0 = y).
+    { apply x_plus0. }
+    rewrite H.
+    reflexivity.
+  - (* Case: S x + y = y + S x *)
+    simpl.
+    assert (H: y + S x = S (y + x)).
+    { apply x_plusSy. }
+    rewrite H.
+    rewrite IHx.
+    reflexivity.
+Qed.
 
 (**
 
@@ -139,8 +197,11 @@ auxiliary results. You can use Admitted theorems.
 Theorem ex9:
   forall x y n, x + n = y + n -> x = y.
 Proof.
-
-Admitted.
+intros.
+rewrite ex8 in H. rewrite (ex8 y) in H. apply ex7 in H.
+rewrite H.
+reflexivity.
+Qed.
 
 
 
